b2d0f05e87a73e132f86f4bc690948acc69031e5
fjmiguel <fernandoj.miguel@iesnervion.es>
2023-11-02 13:59:23 +0100

    terminar 4

---
+++ EjerciciosUd8/Ejercicio4Ud8/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio4Ud8
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ EjerciciosUd8/Ejercicio4Ud8/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio4Ud8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
